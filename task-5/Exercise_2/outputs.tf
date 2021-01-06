1d1e53ff34cb3b915f78933f7f39f6db7a079787
Mateus Costa <mateusvoc@gmail.com>
2021-01-05 23:03:30 -0300

    Includes lambda outputs

--- task-5/Exercise_2/outputs.tf
+++ task-5/Exercise_2/outputs.tf
@@ -1,1 +1,5 @@
 # TODO: Define the output variable for the lambda function.
+output "function_invoke_arn" {
+  description = "The Invoke ARN of the Lambda function"
+  value       = aws_lambda_function.greet_lambda.invoke_arn
+}
